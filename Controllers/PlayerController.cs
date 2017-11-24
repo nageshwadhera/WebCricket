c9772c0ad6943de2dd0e2724f0e3dc04683a0aeb
nageshwadhera <33040426+nageshwadhera@users.noreply.github.com>
2017-11-24 23:06:08 +0530

    Add files via upload

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using cricket.Models;
+
+namespace cricket.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        SqlConnection Connection = new SqlConnection("Server = .\\SQLEXPRESS; Initial Catalog=dbCricket; Integrated Security= sspi;");
+        SqlCommand Command;
+     //   SqlDataReader Reader;
+        String Query = "";
+        Int32 RowsAffected = 0;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddPlayer()
+        {
+            return View();
+        }
+        public ActionResult Teams()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult India()
+        {
+            return View();
+        }
+        public ActionResult Australia()
+        {
+            return View();
+        }
+        public ActionResult Southafrica()
+        {
+            return View();
+        }
+        public ActionResult England()
+        {
+            return View();
+        }
+
+        public ActionResult Nz()
+        {
+            return View();
+        }
+        public ActionResult SriLanka()
+        {
+            return View();
+        }
+        public ActionResult Pakistan()
+        {
+            return View();
+        }
+        public ActionResult WestIndies()
+        {
+            return View();
+        }
+        public ActionResult Bangladesh()
+        {
+            return View();
+        }
+        public ActionResult Zimbabwe()
+        {
+            return View();
+        }
+        public ActionResult Afghanistan()
+        {
+            return View();
+        }
+        public ActionResult Photos()
+        {
+            return View();
+        }
+        public ActionResult Rankings()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ModelPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cricket.Models
+{
+    public class ModelPlayer
+    {
+        public String PlayerName { get; set; }
+        public String FatherName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Int64 MobileNo { get; set; }
+        public Int32 Height { get; set; }
+        public String ResAddress { get; set; }
+        public String Email { get; set; }
+        public String SchoolName { get; set; }
+        public Int64 SchContact { get; set; }
+        public String Sex { get; set; }
+        public Int32 PWeight { get; set; }
+        public Int32 TShirtSize { get; set; }
+
+    }
+}
